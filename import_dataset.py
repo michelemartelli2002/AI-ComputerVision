939ef5a600db02db75e4386cf9790726fbc3a11a
Michele Martelli <michele.martelli1@studenti.unipr.it>
2025-05-27 12:30:39 +0200

    Import dataset script for fine tuning

---
+++ import_dataset.py
@@ -0,0 +1,14 @@
+import os
+import kaggle
+
+# Requirement: Kaggle API key in ~/.kaggle/kaggle.json
+#              pip kaggle package
+
+dataset_id = "ma7555/cat-breeds-dataset"
+out_folder = ".data/cat-breeds-dataset"
+os.makedirs(out_folder, exist_ok=True)
+
+print(f"Downloading dataset...")
+kaggle.api.dataset_download_files(dataset=dataset_id, path=out_folder, unzip=True, quiet=False)
+
+print("Download and extraction complete.")
