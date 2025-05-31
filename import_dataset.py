8b30ab46216c4934cfbeed0c5ede3c21332b9d7d
Michele Martelli <michele.martelli1@studenti.unipr.it>
2025-05-31 15:55:12 +0200

    Updated dataset to upload

--- import_dataset.py
+++ import_dataset.py
@@ -3,9 +3,8 @@ import kaggle
 
 # Requirement: Kaggle API key in ~/.kaggle/kaggle.json
 #              pip kaggle package
-
-dataset_id = "ma7555/cat-breeds-dataset"
-out_folder = ".data/cat-breeds-dataset"
+dataset_id = "zippyz/cats-and-dogs-breeds-classification-oxford-dataset"
+out_folder = ".data/oxford-cats-dogs"
 os.makedirs(out_folder, exist_ok=True)
 
 print(f"Downloading dataset...")
